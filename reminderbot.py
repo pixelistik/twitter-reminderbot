346880329a31661b5b42318589ce94b7666558a1
Pixelistik <code@pixelistik.de>
2015-09-16 21:17:41 +0200

    Set up working Twitter OAuth proof

--- reminderbot.py
+++ reminderbot.py
@@ -1,5 +1,42 @@
 import random
+import tweepy
+import click
 
-class Reminderbot():
-    def _get_random_quote(self):
-        return random.choice(self._quotes)
+def _get_random_quote():
+    return random.choice(   _quotes)
+
+@click.command()
+@click.argument("consumer_key")
+@click.argument("consumer_secret")
+def authenticate(consumer_key, consumer_secret):
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
+    try:
+        url = auth.get_authorization_url()
+    except tweepy.TweepError:
+        click.echo("Failed to get request token.")
+
+    click.echo("Setting up authentication. Please visit this URL:")
+    click.echo(url)
+    verifier = click.prompt("Enter the authorisation PIN from Twitter:")
+
+    try:
+        auth.get_access_token(verifier)
+    except tweepy.TweepError:
+        click.echo("Error! Failed to get access token.")
+
+
+    api = tweepy.API(auth)
+
+    public_tweets = api.home_timeline()
+    for tweet in public_tweets:
+        click.echo(tweet.text)
+
+@click.group()
+def cli():
+    pass
+
+cli.add_command(authenticate)
+
+if __name__ == '__main__':
+    cli()
--- tests.py
+++ tests.py
@@ -2,31 +2,55 @@ import unittest
 import logging
 import random
 from mock import patch, Mock
-from reminderbot import Reminderbot
+import reminderbot
+import tweepy
+import click
+from click.testing import CliRunner
 
 class TestReminderbot(unittest.TestCase):
-    def setUp(self):
-        self.reminderbot = Reminderbot()
-
-    def test_instantiation(self):
-        result = Reminderbot()
+    def __init__(self):
+        self.open_name = "%s.open" % __name__
 
     def test_get_random_quote_single(self):
-        self.reminderbot._quotes = [
+        reminderbot._quotes = [
             "A quote"
         ]
-        result = self.reminderbot._get_random_quote()
+        result = reminderbot._get_random_quote()
         self.assertEqual(result, "A quote")
 
     @patch("random.choice", Mock(side_effect=lambda quotes: quotes[0]))
     def test_get_random_quote_multi(self):
-        self.reminderbot._quotes = [
+        reminderbot._quotes = [
             "A first quote",
             "A second quote"
         ]
 
-        result = self.reminderbot._get_random_quote()
+        result = reminderbot._get_random_quote()
         self.assertEqual(result, "A first quote")
+
+    @patch("tweepy.OAuthHandler", Mock())
+    @patch("click.prompt", Mock(return_value="12345"))
+    @patch(self.open_name, Mock())
+    def test_authenticate(self):
+        auth = Mock()
+        tweepy.OAuthHandler.return_value = auth
+
+        token_file = Mock()
+        open.return_value = token_file
+
+        auth.access_token = "token"
+        auth.access_token_secret = "token_secret"
+
+        runner = CliRunner()
+        result = runner.invoke(reminderbot.authenticate, ["key", "secret"])
+
+        tweepy.OAuthHandler.assert_called_once_with("key", "secret")
+        auth.get_authorization_url.assert_called_once_with()
+        auth.get_access_token.assert_called_once_with("12345")
+
+        token_file.assert_called_once_with("token\ntoken_secret")
+
+
 
 if __name__ == '__main__':
 	logging.basicConfig(level=logging.DEBUG)
